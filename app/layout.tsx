ddf9c0058a05dded19c5bcf3c213dab7c21da5d2
sam00002135 <104470583+sam00002135@users.noreply.github.com>
2024-03-28 10:36:30 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Toaster } from 'sonner';
 import './globals.css'
-import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider } from '@/components/theme-provider'
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,7 +24,6 @@ export default function RootLayout({
 
             <Toaster theme="light" position="bottom-center" />
             {children}
-            <Analytics />
         </body>
       </html>
     </ClerkProvider>
